d2396cc4cb82344052b88500e26e4be2c7c4fed8
Yunus Adebayo <68831309+Adeyunze@users.noreply.github.com>
2022-03-20 16:05:12 +0100

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -85,7 +85,7 @@ const Work = () => {
           <div className="vid-cont flex lg:flex-row flex-col">
             <div className="my-vid bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
               <video autoPlay muted loop className=''>
-                <source src={`https://i.imgur.com/d1Ehu3b.mp4`} />
+                <source src={`https://i.imgur.com/eUvRabw.mp4`} />
               </video>
             </div>
             <div className="right-vid lg:ml-16 lg:w-[50%] mt-5">
@@ -107,7 +107,7 @@ const Work = () => {
           <div className="vid-cont flex lg:flex-row flex-col">
             <div className="my-vid1 bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
               <video autoPlay muted loop className=''>
-                <source src={`https://i.imgur.com/eUvRabw.mp4`} />
+                <source src={`https://i.imgur.com/d1Ehu3b.mp4`} />
               </video>
             </div>
             <div className="right-vid lg:ml-16 lg:w-[50%] mt-5">
@@ -129,7 +129,7 @@ const Work = () => {
           <div className="vid-cont flex lg:flex-row flex-col">
             <div className="my-vid2 bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
               <video autoPlay muted loop className=''>
-                <source src={vid3} />
+                <source src={`https://i.imgur.com/ssTSfRc.mp4`} />
               </video>
             </div>
             <div className="right-vid lg:ml-16 lg:w-[50%] mt-5">
