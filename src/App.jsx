784b5fc2ec9db4463ac982c8f83f90a0f230ea8d
Yunus Adebayo <68831309+Adeyunze@users.noreply.github.com>
2022-03-20 14:16:12 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './index.css';
+import Navbar from './components/Navbar';
+import ImageSection from './components/ImageSection';
+import Skills from './components/Skills';
+import Work from './components/Work';
+import Footer from './components/Footer';
+// import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <div className='md:px-16 px-5'>
+      <Navbar/>
+      <ImageSection/>
+      <Skills/>
+      <Work/>
+    </div>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
