2a0a98a29a78afca932d6f34b0617639326e5f4f
Adeyunze <adeyunze05@gmail.com>
2022-10-25 09:00:16 +0100

    Added recent Projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -83,6 +83,31 @@ const Work = () => {
           <div className="vid-cont flex lg:flex-row flex-col">
             <div className="my-vid bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
               <video autoPlay muted loop className=''>
+                <source src={`https://imgur.com/7PDsjfV`} />
+              </video>
+            </div>
+            <div className="right-vid lg:ml-16 lg:w-[50%] mt-5">
+                <a href="https://yands.vercel.app/" target={`_blank`} className='flex items-center hover:text-[#7794FF] ease-in duration-300'>
+                  <h1 className='lg:text-3xl text-2xl font-medium'>Y & S (Fullstack Ecommerce store)</h1>
+                  <BsArrowUpRightCircle className='lg:text-3xl text-2xl ml-5'/>
+                </a>
+                <p className='mt-5 text-lg font-light'>This project was built using NodeJS, React and TailwindCSS, this project aimed to Build a fully functional and interactive Ecommerce store</p>
+                <div className="tags mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
+                  <span className='text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3'>NodeJS</span>
+                  <span className='text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3'>MongoDB</span>
+                  <span className='text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3'>Mongoose</span>
+                  <span className='text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]'>REACT</span>
+                  <span className='text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3'>TAILWIND</span>
+                  <span className='text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3'>FIGMA</span>
+                </div>
+            </div>
+          </div>
+      </div>
+      <div className="flex-vid mb-32 lg:mb-40">
+          <div className="line w-full h-[1px] bg-white mb-14"></div>
+          <div className="vid-cont flex lg:flex-row flex-col">
+            <div className="my-vid bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
+              <video autoPlay muted loop className=''>
                 <source src={`https://i.imgur.com/eUvRabw.mp4`} />
               </video>
             </div>
