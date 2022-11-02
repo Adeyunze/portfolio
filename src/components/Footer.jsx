1ca0606e381fb553a664e226efa908f4e0d7dd90
Adeyunze <adeyunze05@gmail.com>
2022-11-02 13:01:25 +0000

    changed header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,6 @@ const Footer = () => {
         <div className="socials md:w-[50%] md:ml-32 mt-12 md:mt-28">
           <h1 className='text-2xl'>Find me on my socials!!!😁</h1>
           <div className="social-links flex items-center mt-5 md:mt-12">
-            <a href="https://twitter.com/_adeyunze" className='mr-5' target={`_blank`}>
-              <FaTwitter className='text-4xl'/>
-            </a>
             <a href="https://adeyunze.hashnode.dev/" className='mr-5' target={`_blank`}>
               <SiHashnode className='text-4xl'/>
             </a>
