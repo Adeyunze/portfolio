668405b059351febeb2fad5742b52e9686067604
Adeyunze <adeyunze05@gmail.com>
2023-07-23 10:10:41 +0100

    Updated footer with works

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,82 +1,88 @@
-import { React, useRef, useEffect } from 'react';
-import pics from '../assets/pics.png'
-import { BsArrowUpRightCircle } from 'react-icons/bs';
-import gsap from 'gsap';
+import { React, useRef, useEffect } from "react";
+import pics from "../assets/pics.png";
+import { BsArrowUpRightCircle } from "react-icons/bs";
+import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import worldstunner from '../assets/worldstunner.png'
+import worldstunner from "../assets/worldstunner.png";
+import yandz from "../assets/yandz.png";
 
 const Work = () => {
-  gsap.registerPlugin(ScrollTrigger);
-  const workref = useRef(null);
+	gsap.registerPlugin(ScrollTrigger);
+	const workref = useRef(null);
 
-  useEffect(() => {
-    gsap.fromTo(".line", {
-      width: 0,
-    }, 
-    {
-      duration: 2,
-      width: '100%',
-      scrollTrigger: {
-        trigger: '.my-vid',
-      }
-    }
-    
-    );
+	useEffect(() => {
+		gsap.fromTo(
+			".line",
+			{
+				width: 0,
+			},
+			{
+				duration: 2,
+				width: "100%",
+				scrollTrigger: {
+					trigger: ".my-vid",
+				},
+			}
+		);
 
-    gsap.fromTo(".line1", {
-      width: 0,
-    }, 
-    {
-      duration: 2,
-      width: '100%',
-      scrollTrigger: {
-        trigger: '.my-vid1',
-      }
-    }
-    
-    );
+		gsap.fromTo(
+			".line1",
+			{
+				width: 0,
+			},
+			{
+				duration: 2,
+				width: "100%",
+				scrollTrigger: {
+					trigger: ".my-vid1",
+				},
+			}
+		);
 
-    gsap.fromTo(".line2", {
-      width: 0,
-    }, 
-    {
-      duration: 2,
-      width: '100%',
-      scrollTrigger: {
-        trigger: '.my-vid2',
-      }
-    }
-    
-    )
+		gsap.fromTo(
+			".line2",
+			{
+				width: 0,
+			},
+			{
+				duration: 2,
+				width: "100%",
+				scrollTrigger: {
+					trigger: ".my-vid2",
+				},
+			}
+		);
 
-    gsap.fromTo(".line3", {
-      width: 0,
-    }, 
-    {
-      duration: 2,
-      width: '100%',
-      scrollTrigger: {
-        trigger: '.my-vid3',
-      }
-    }
-    
-    );
+		gsap.fromTo(
+			".line3",
+			{
+				width: 0,
+			},
+			{
+				duration: 2,
+				width: "100%",
+				scrollTrigger: {
+					trigger: ".my-vid3",
+				},
+			}
+		);
 
-  gsap.fromTo(".line4", {
-      width: 0,
-    }, 
-    {
-      duration: 2,
-      width: '100%',
-      scrollTrigger: {
-        trigger: '.my-vid4',
-      }
-    }
-    
-    )
-  }, [])
+		gsap.fromTo(
+			".line4",
+			{
+				width: 0,
+			},
+			{
+				duration: 2,
+				width: "100%",
+				scrollTrigger: {
+					trigger: ".my-vid4",
+				},
+			}
+		);
+	}, []);
 
-  return (
+	return (
 		<section className="projects-container pt-20" id="work" ref={workref}>
 			<h1 className="lg:text-7xl md:text-3xl text-2xl mb-20 lg:mb-40">
 				Recent work
@@ -106,23 +112,23 @@ const Work = () => {
 							project aimed to Build a fully functional and interactive
 							Ecommerce store
 						</p>
-						<div className="tags mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+						<div className="tags mt-10 flex items-center flex-wrap mr-[-3px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								NodeJS
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								MongoDB
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								Mongoose
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
 								REACT
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								TAILWIND
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								FIGMA
 							</span>
 						</div>
@@ -147,21 +153,56 @@ const Work = () => {
 							<BsArrowUpRightCircle className="lg:text-3xl text-2xl ml-5" />
 						</a>
 						<p className="mt-5 text-lg font-light"></p>
-						<div className="tags mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+						<div className="tags mt-10 flex items-center flex-wrap mr-[-3px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								SHOPIFY
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								LIQUID
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
 								REACT
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								SWIPERJS
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								FIGMA
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="flex-vid mb-32 lg:mb-40">
+				<div className="line1 w-full h-[1px] bg-white mb-14"></div>
+				<div className="vid-cont flex lg:flex-row flex-col">
+					<div className="my-vid1 bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
+						<img src={yandz} alt="yandz furniture store" />
+					</div>
+					<div className="right-vid lg:ml-16 lg:w-[50%] mt-5">
+						<a
+							href="https://yandz.shop/"
+							target={`_blank`}
+							className="flex items-center hover:text-[#7794FF] ease-in duration-300"
+						>
+							<h1 className="lg:text-2xl tracking-wide font-medium uppercase">
+								Yandz furniture store
+							</h1>
+							<BsArrowUpRightCircle className="lg:text-3xl text-2xl ml-5" />
+						</a>
+						<p className="mt-5 text-lg font-light"></p>
+						<div className="tags lg:mt-10 flex items-center flex-wrap mr-[-3px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
+								SHOPIFY
+							</span>
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+								LIQUID
+							</span>
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+								JAVASCRIPT
+							</span>
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+								REACT
 							</span>
 						</div>
 					</div>
@@ -187,92 +228,21 @@ const Work = () => {
 							<BsArrowUpRightCircle className="lg:text-3xl text-2xl ml-5" />
 						</a>
 						<p className="mt-5 text-lg font-light"></p>
-						<div className="tags mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
+						<div className="tags mt-10 flex items-center flex-wrap mr-[-3px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
 								REACT
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								TAILWIND
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								FIGMA
 							</span>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="flex-vid mb-32 lg:mb-40">
-				<div className="line1 w-full h-[1px] bg-white mb-14"></div>
-				<div className="vid-cont flex lg:flex-row flex-col">
-					<div className="my-vid1 bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
-						<video autoPlay muted loop className="">
-							<source src={`https://i.imgur.com/d1Ehu3b.mp4`} />
-						</video>
-					</div>
-					<div className="right-vid lg:ml-16 lg:w-[50%] mt-5">
-						<a
-							href="https://yunze-rps.netlify.app/"
-							target={`_blank`}
-							className="flex items-center hover:text-[#7794FF] ease-in duration-300"
-						>
-							<h1 className="lg:text-2xl tracking-wide font-medium uppercase">
-								RPS GAME
-							</h1>
-							<BsArrowUpRightCircle className="lg:text-3xl text-2xl ml-5" />
-						</a>
-						<p className="mt-5 text-lg font-light">
-							This game was an "Advance" Challenge on front end mentor which
-							entails using HTML, CSS, and JavaScript to build a rock paper
-							scissors game
-						</p>
-						<div className="tags lg:mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
-								HTML
-							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
-								CSS
-							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
-								JAVASCRIPT
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="flex-vid mb-32 lg:mb-40">
-				<div className="line2 w-full h-[1px] bg-white mb-14"></div>
-				<div className="vid-cont flex lg:flex-row flex-col">
-					<div className="my-vid2 bg-[#445AAB] w-[100%] px-5 lg:px-16 py-4 lg:py-9">
-						<video autoPlay muted loop className="">
-							<source src={`https://i.imgur.com/ssTSfRc.mp4`} />
-						</video>
-					</div>
-					<div className="right-vid lg:ml-16 lg:w-[50%] mt-5">
-						<a
-							href="https://yunus-countries-api.netlify.app/"
-							target={`_blank`}
-							className="flex items-center hover:text-[#7794FF] ease-in duration-300"
-						>
-							<h1 className="lg:text-2xl tracking-wide font-medium uppercase">
-								COUNTRIES API
-							</h1>
-							<BsArrowUpRightCircle className="lg:text-3xl text-2xl ml-5" />
-						</a>
-						<p className="mt-5 text-lg font-light"></p>
-						<div className="tags mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
-								REACT
-							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
-								TAILWINDCSS
-							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
-								API
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
+
 			<div className="flex-vid mb-32 lg:mb-40">
 				<div className="line3 w-full h-[1px] bg-white mb-14"></div>
 				<div className="vid-cont flex lg:flex-row flex-col">
@@ -293,14 +263,14 @@ const Work = () => {
 							<BsArrowUpRightCircle className="lg:text-3xl text-2xl ml-5" />
 						</a>
 						<p className="mt-5 text-lg font-light"></p>
-						<div className="tags mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
+						<div className="tags mt-10 flex items-center flex-wrap mr-[-3px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
 								HTML
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								CSS
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								JAVASCRIPT
 							</span>
 						</div>
@@ -324,14 +294,14 @@ const Work = () => {
 							This project was a freelance project which entails building the
 							company website from scratch using Bootstrap.
 						</p>
-						<div className="tags mt-10 flex items-center flex-wrap mt-[-2px] mr-[-3px]">
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
+						<div className="tags mt-10 flex items-center flex-wrap mr-[-3px]">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px]">
 								HTML
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								BOOTSTRAP
 							</span>
-							<span className="text-base text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
+							<span className="text-slate-300 px-7 py-2 border rounded-full text-sm tracking-widest mt-[15px] lg:mr-[3px] mr-[15px] lg:ml-3">
 								JAVASCRIPT
 							</span>
 						</div>
@@ -340,6 +310,6 @@ const Work = () => {
 			</div>
 		</section>
 	);
-}
+};
 
-export default Work
+export default Work;
